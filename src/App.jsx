bfba715ee686c1f2bfdea92071b92250a42f497c
Franz Seidel <franzseidel@MacBook-Air-de-Franz.local>
2023-10-25 14:07:03 -0500

    responsive -1024px

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       {/* seccion Hero */}
       <section className="hero align-items-stretch">
         <div className="hero-principal d-flex justify-content-center row py-5">
-          <div className="col-12 col-md-6 py-5">
+          <div className="col-12 col-md-6 py-1">
             <h5>Franz Seidel</h5>
             <h1>Desarrollador Frontend & Diseñador web</h1>
             <p className="mt-5">
--- src/components/proyects.jsx
+++ src/components/proyects.jsx
@@ -5,7 +5,7 @@ function proyects() {
         <div className="container">
           <div className="row proyects-container">
             <h2>Proyectos</h2>
-            <div className="col-12 col-md-8 col-lg-4">
+            <div className="col-12 py-4">
               <div className="card">
                 <img className="img-proyects" src="src/assets/landing-page-cover.png" alt="Where2Day" /> {/* img-fluid -> ADAPTA LA IMAGEN AL RESPONSIVE  */}
                 <div className="card-body">
@@ -15,7 +15,7 @@ function proyects() {
                 </div>
               </div>
             </div>
-            <div className="col-12 col-md-8 col-lg-4">
+            <div className="col-12 py-4">
               <div className="card">
                 <img className="img-proyects img-fluid" src="https://picsum.photos/200" alt="StarWars blog" />
                 <div className="card-body">
@@ -24,7 +24,7 @@ function proyects() {
                 </div>
               </div>
             </div>
-            <div className="col-12 col-md-8 col-lg-4">
+            <div className="col-12 py-4">
               <div className="card">
                 <img className="img-proyects img-fluid" src="https://picsum.photos/200" alt="StarWars API Fetch" />
                 <div className="card-body">
